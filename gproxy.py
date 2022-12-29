5e752e95193834958298742dda8e619887348d44
modbots <77435462+modbots@users.noreply.github.com>
2022-12-30 00:25:42 +0630

    initial released

---
+++ gproxy.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+import requests
+from urllib import request, error
+import socket
+import time
+import math
+import os
+
+socket.setdefaulttimeout(60)
+mirror = 'http://speedtest.tele2.net/1MB.zip'
+filename = '1.zip'
+
+
+allproxies = [
+    "https://raw.githubusercontent.com/mertguvencli/http-proxy-list/main/proxy-list/data.txt",
+    "https://raw.githubusercontent.com/UptimerBot/proxy-list/main/proxies/http.txt",
+    "https://raw.githubusercontent.com/almroot/proxylist/master/list.txt",
+    "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-http.txt",
+]
+def proxies():
+    for i in allproxies:
+        #proxies(i)
+        print(i)
+    #url = "https://raw.githubusercontent.com/UptimerBot/proxy-list/main/proxies/http.txt"#"https://www.proxyscan.io/download?type=http"
+        url =  i#"https://raw.githubusercontent.com/mertguvencli/http-proxy-list/main/proxy-list/data.txt"
+        proxis = requests.get(url).text
+        proxis = proxis.splitlines()
+        for proxy in proxis:
+            prox,port = proxy.split(':')
+            #print(f'checking {proxy}...')
+            response = requests.post(f"http://ip-api.com/json/{prox}").json()
+            try:
+                res = response['isp']
+                #print(res)
+                if res == 'Google LLC':
+                    #print(proxy)
+                    r = requests.get("https://google.com/",proxies= {'http': proxy,'https': proxy}, timeout=5)
+                    #print(r.status_code)
+                    stat = r.status_code
+                    if stat == 200:
+                        
+                        proxycheck = request.ProxyHandler({'http': proxy})
+                        opener = request.build_opener(proxycheck)
+                        request.install_opener(opener)
+                        start = time.time()
+                        request.urlretrieve(mirror, filename)
+                        delta = time.time() - start
+                        filesize = os.path.getsize(filename)
+                        ratio = filesize/(delta*1024)
+                        print('GProxy {} has download speed:{} KB/s'.format(proxy, math.floor(ratio)))
+                        if os.path.isfile(filename):
+                            os.remove(filename)
+                        
+                    else:
+                        pass
+
+                else:
+                    pass
+
+            except:
+                pass
+
+
+proxies()
+
+print("finished")
